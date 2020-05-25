2c54bf25f27500210810ecb6a6052c0a2ee4922c
DavidSint <david@sint.dev>
2020-05-25 01:21:23 +0100

    Updates

---
+++ precache-manifest.c860ddde2796c0a0bcf26da5e68d7db9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7013b472741b85618b393a88e030748f",
+    "url": "/dnd-dice/index.html"
+  },
+  {
+    "revision": "ff9e80bceff481436a59",
+    "url": "/dnd-dice/static/css/main.31f36077.chunk.css"
+  },
+  {
+    "revision": "40115e7832a780b0caa7",
+    "url": "/dnd-dice/static/js/2.3e8e42e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dnd-dice/static/js/2.3e8e42e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff9e80bceff481436a59",
+    "url": "/dnd-dice/static/js/main.549c7777.chunk.js"
+  },
+  {
+    "revision": "b0f14429ae4d07e820dd",
+    "url": "/dnd-dice/static/js/runtime-main.f5358443.js"
+  }
+]);
