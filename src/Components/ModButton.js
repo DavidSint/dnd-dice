41b7a286e81d23373ededcef991ec6a4c827b557
David Sint <david@sint.dev>
2021-03-08 03:20:00 +0000

    Feat: Right-click mod button to reset

--- src/Components/ModButton.js
+++ src/Components/ModButton.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 function ModButton({ myMod, setMyMod }) {
   return (
-    <button className="mod" tabIndex={0} onClick={() => {
+    <button
+      className="mod"
+      tabIndex={0}
+      onContextMenu={((e) => {
+        e.preventDefault();
+        setMyMod(0)
+      })}
+      onClick={() => {
       const modifier = prompt("Please enter a modifier");
       if (modifier){
         setMyMod(parseInt(modifier, 10))
